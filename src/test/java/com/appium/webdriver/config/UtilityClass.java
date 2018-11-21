11fe85b75aa0bbcc4ac72701190610fb9ece3e8f
unknown <shiveshyadav@gmail.com>
2018-11-21 22:23:39 +0530

    update utility class

--- src/test/java/com/appium/webdriver/config/UtilityClass.java
+++ src/test/java/com/appium/webdriver/config/UtilityClass.java
@@ -1,7 +1,6 @@
 package com.appium.webdriver.config;
 
 import java.util.NoSuchElementException;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
